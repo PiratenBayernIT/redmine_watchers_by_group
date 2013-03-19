cabf8a54ebef388a971a4751440f76c9f39e4af5
Tobias Stenzel <escaP@gmx.de>
2013-03-19 04:26:37 +0100

    German translation, moved content to watchers box

--- lib/hooks/hooks.rb
+++ lib/hooks/hooks.rb
@@ -1,5 +1,5 @@
 module Redmine_watchers_by_group
   class Hooks < Redmine::Hook::ViewListener
-    render_on :view_issues_form_details_bottom, :partial => 'watchers/multiselect_group'
+    render_on :view_issues_watchers_bottom, :partial => 'watchers/multiselect_group'
   end
 end
